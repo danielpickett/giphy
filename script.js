8900b96e8d1c5142bc61817d2c972a5042bb60d6
Daniel Pickett <daniel@danielpickett.me>
2018-09-28 22:56:15 -0400

    added lightbox feature for images with a larger size available.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $("#search-input").focus();
 
-var query;
-var result;
-var offset = 0;
-var queryUrl; 
+let query;
+let result;
+let offset = 0;
 
 document.getElementById('search-input').addEventListener('keyup', function(event){
   event.preventDefault();
@@ -18,16 +17,48 @@ document.getElementById('search-input').addEventListener('keyup', function(event
 
 
 function getGifs(){
-  
-  var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=z2aAdK0B5G97BdECKFeHRER9OBWFlcNb&limit=30&offset=" + offset);
+  let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=z2aAdK0B5G97BdECKFeHRER9OBWFlcNb&limit=30&sort=relevant&offset=" + offset;
+  console.log(queryUrl);
+
+  var xhr = $.get(queryUrl);
   xhr.done(function(data) { 
     
-    console.log("success got data", data);
+    console.log(data);
     for (item of data.data) {
+      let html = '<div class="img-wrapper"><img width="' + item.images.fixed_height.width + '" height="' + item.images.fixed_height.height + '" src="' + item.images.fixed_height.url + '">';
+      if (item.images.original.height > (parseInt(item.images.fixed_height.height) + parseInt(15))) {
+        console.log(item.images.original.height + ' > ' + (parseInt(item.images.fixed_height.height) + parseInt(15)));
+        html = html + '<span onclick="openLightbox(\'' + item.images.original.url + '\', \'' + item.images.original.width + '\', \'' + item.images.original.height + '\');">+</span>';
+      }
       
-      $('#output').append('<div class="img-wrapper"><img width="' + item.images.fixed_height_downsampled.width + '" height="' + item.images.fixed_height_downsampled.height + '" src="' + item.images.fixed_height_downsampled.url + '"></div>');
+      html = html + '</div>';
+
+      $('#output').append(html);
+      
     }
     $('#load-more').show();
     offset = offset + 30;
   });
 }
+
+function openLightbox(imgUrl, imgWidth, imgHeight) {
+  let html = '<div class="lightbox-img-wrapper"><img src="' + imgUrl + '" height="' + imgHeight + '" width="' + imgWidth + '"></div>';
+  let lightbox = document.getElementById('lightbox');
+  lightbox.innerHTML = html;
+  lightbox.style.display = 'flex';
+  document.querySelector('#lightbox img').addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    event.stopPropagation();
+  });
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').innerHTML = '';
+  document.getElementById('lightbox').style.display = 'none';
+}
+
+document.getElementById('lightbox').addEventListener('click', () => {
+  closeLightbox();
+});
+
