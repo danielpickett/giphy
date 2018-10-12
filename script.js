81421449c1252c6cd2c81f5f3bc944830549b10e
Daniel Pickett <daniel@danielpickett.me>
2018-10-11 22:27:00 -0400

    full incremental load implemented

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
+$('#loading-text').remove();
+
+let initialHtml = '<div id="lightbox" style="display: none;"></div><div id="input"><input type="text" id="search-input" value="" placeholder="search gifs"></div><div id="output"></div><div id="load-more" onclick="getGifs()"><span>Load more</span></div>';
+$(document.body).prepend(initialHtml);
+
+// debugger;
 let $output = $('#output');
 let $input = $('#search-input');
 //$input.focus();
+
+
 
 let query;
 let result;
@@ -16,7 +24,6 @@ $input.on('keyup', function(event){
     console.log(query);
     $output.html('');
     getGifs();
-    
   }
 });
 
@@ -34,18 +41,30 @@ function getGifs(){
     //loadImage(response, 0);
     console.log(response);
     buildSkeleton(response);
-  });
+  }); 
 }
 
 function buildSkeleton(imageCollection) {
   console.log(imageCollection)
   for (let i = 0; i < imageCollection.data.length; i++) {
-    $output.append('<div class="img-wrapper" id="' + imageCollection.data[i].id + '" style="height:' + imageCollection.data[i].images.fixed_height.height + 'px; width: ' + imageCollection.data[i].images.fixed_height.width + 'px;" data-img-loaded="false"></div>');
+    let data = imageCollection.data[i];
+    let images = data.images;
+    let html = '<div class="img-wrapper" id="' + data.id + '" style="height:' + images.fixed_height.height + 'px; width: ' + images.fixed_height.width + 'px;" data-img-loaded="false">'
+    
+    if (images.original.height > (parseInt(images.fixed_height.height) + parseInt(15))) {
+      console.log(images.original.height + ' > ' + (parseInt(images.fixed_height.height) + parseInt(15)));
+      html = html + '<span onclick="openLightbox(\'' + images.original.url + '\', \'' + images.original.width + '\', \'' + images.original.height + '\');">+</span>';
+    }
+    html = html + '</div>'
+    $output.append(html);
   }
   loadingSweep();
+  $('#load-more').show();
+  offset = offset + 30;
 }
 
 function loadingSweep() {
+  // debugger;
   let $arr = $('.img-wrapper[data-img-loaded=false]');
   for (let i = 0; i < $arr.length; i++) {
     console.log($arr[i]);
@@ -56,51 +75,37 @@ function loadingSweep() {
       this.parentElement.setAttribute('data-img-loaded', 'true');
     };
     $arr[i].append(img);
-    $arr[i].setAttribute('data-load-status', 'loading');
+    $arr[i].setAttribute('data-img-loaded', 'loading');
+
   }
 }
 
 function cancel() {
   window.stop();
+  console.log('window stopped');
+  $('.img-wrapper[data-img-loaded=loading]').attr('data-img-loaded', 'false');
   $('.img-wrapper[data-img-loaded=false] img').remove('');
 }
 
 
 
-
-
-
-
-function loadImage(imageCollection, $imgWrapperArr, i){
-  console.log('running loadImage');
-  let img = document.createElement('img');
-  img.src = imageCollection.data[i].images.fixed_height.url;
-  img.onload = function() {
-    loadImage(imageCollection, $('.img-wrapper[data-load-status=empty]'), ++i);
-  };
-  $imgWrapperArr[i].append(img);
-  $imgWrapperArr[i].setAttribute('data-load-status', 'loading');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // LIGHTBOX FUNCTIONS
 function openLightbox(imgUrl, imgWidth, imgHeight) {
-  let html = '<div class="lightbox-img-wrapper"><img src="' + imgUrl + '" height="' + imgHeight + '" width="' + imgWidth + '"></div>';
+  cancel();
+  //<img src="' + imgUrl + '" height="' + imgHeight + '" width="' + imgWidth + '">
+  let html = '<div class="lightbox-img-wrapper"></div>';
   let lightbox = document.getElementById('lightbox');
   lightbox.innerHTML = html;
   lightbox.style.display = 'flex';
+
+  let img = document.createElement('img');
+  img.src = imgUrl;
+  img.height = imgHeight;
+  img.width = imgWidth;
+  img.onload = function(){
+    loadingSweep();
+  };
+  $('.lightbox-img-wrapper').append(img);
   document.querySelector('#lightbox img').addEventListener('click', (event) => {
     event.preventDefault();
     event.stopImmediatePropagation();
@@ -109,8 +114,10 @@ function openLightbox(imgUrl, imgWidth, imgHeight) {
 }
 
 function closeLightbox() {
+  cancel();
   document.getElementById('lightbox').innerHTML = '';
   document.getElementById('lightbox').style.display = 'none';
+  loadingSweep();
 }
 
 document.getElementById('lightbox').addEventListener('click', () => {
